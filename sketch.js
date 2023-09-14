7bc65adf2bb9a2f39ce5093cd7069a3f469fc782
luuib <129198298+luuib@users.noreply.github.com>
2023-09-14 07:55:20 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,20 @@
+var num1= 100;
+var num2= 10;
+
+///console.log (num1=num2)
+
+var soma
+
+function calcularSoma (num1,num2) {
+  soma= num1 + num2;
+  return soma;
+}
+
+resultado = calcularSoma(num1,num2);
+console.log('A soma é',num1 ,'e' ,num2,'é',resultado )
+
+num1=300;
+num2=40;
+
+resultado = calcularSoma(num1,num2);
+console.log("A soma é",resultado )
